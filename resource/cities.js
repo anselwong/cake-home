f428d70f8d1af1a8052f0c83e0149ee31ed91029
AnselWong <f2e.ansel@gmail.com>
2019-05-12 13:02:26 +0800

    Update

---
+++ resource/cities.js
@@ -0,0 +1,13 @@
+var cities = [{
+  value: 'https://h5.youzan.com/v2/feature/VlLbMob5rL',
+  label: '佛山市'
+},{
+  value: 'https://h5.youzan.com/v2/feature/7LwY8vq39V',
+  label: '广州市'
+},{
+  value: 'default',
+  label: '中山市'
+},{
+  value: 'default',
+  label: '珠海市'
+}]
