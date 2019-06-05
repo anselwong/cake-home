952fd9f903a82de6921a26b96046bbcdafede323
AnselWong <f2e.ansel@gmail.com>
2019-06-05 17:15:10 +0800

    Add taishan city

--- resource/cities.js
+++ resource/cities.js
@@ -1,13 +1,16 @@
 var cities = [{
   value: 'https://h5.youzan.com/v2/feature/VlLbMob5rL',
   label: '佛山市'
-},{
+}, {
   value: 'https://h5.youzan.com/v2/feature/7LwY8vq39V',
   label: '广州市'
-},{
+}, {
+  value: 'https://h5.youzan.com/v2/feature/GiekK4VVe4',
+  label: '台山市'
+}, {
   value: 'default',
   label: '中山市'
-},{
+}, {
   value: 'default',
   label: '珠海市'
 }]
